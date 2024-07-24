72bfa5327ac6c4013ab77b83f35a6437da40c131
yogramming <yogeshdubey9336@gmail.com>
2024-07-24 21:50:20 +0530

    Add the logic to javascript file

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 A simple Rock Paper Scissors game that is played on console. This project is made under an assignment of The Odin Project. Thank you for reading it.
 
 Regards
+
 Yogesh Dubey
 (yogramming)
--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-let a = 4;
-let b = 6;
-console.log(a + b);
+function getPlayersCall() {
+    let playersCall = prompt("Choose one out of Rock, Paper or Scissors: ");
+    return playersCall;
+}
+function getComputersCall() {
+    let arr = ["Rock", "Paper", "Scissors"];
+    let index = Math.floor(Math.random() * arr.length);
+    return arr[index];
+}
+// Get player and computer choices
+let playersCall = getPlayersCall();
+let computersCall = getComputersCall();
+
+console.log("Player chose: " + playersCall);
+console.log("Computer chose: " + computersCall);
+
+// Print the result of the game
+let result;
+
+if (playersCall === computersCall) {
+    result = "Match tie!";
+}
+else if (
+    (playersCall === "Rock" && computersCall === "Scissors") ||
+    (playersCall === "Paper" && computersCall === "Rock") ||
+    (playersCall === "Scissors" && computersCall === "Paper")
+) {
+    result = "You won!";
+}
+else {
+    result = "You lost!";
+}
+console.log(result);
